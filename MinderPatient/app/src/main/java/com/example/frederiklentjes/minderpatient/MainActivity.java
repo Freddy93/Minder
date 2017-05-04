ffd9d74d7f0408e30e25153ccf611f8a8b6dde5f
snazimmermann <nadia.zimmermann@rwth-aachen.de>
2017-05-04 10:10:47 +0200

    minderpatient 1.0

--- MinderPatient/app/src/main/java/com/example/frederiklentjes/minderpatient/MainActivity.java
+++ MinderPatient/app/src/main/java/com/example/frederiklentjes/minderpatient/MainActivity.java
@@ -3,30 +3,52 @@ package com.example.frederiklentjes.minderpatient;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+    private ZXingScannerView mScannerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //fab.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+               mScannerView = new ZXingScannerView(this);
+               setContentView(mScannerView);
+               mScannerView.setResultHandler(this);
+               mScannerView.startCamera();
+            }
+        //});
+        @Override
+        protected void onPause(){
+            super.onPause();
+            mScannerView.stopCamera();
+         }
+
+         @Override
+         public void handleResult(Result result){
+             Log.v("handleResult", result.getText());
+             AlertDialog.Builder builder = new AlertDialog.Builder(this);
+             builder.setTitle("Scan result");
+             builder.setMessage(result.getText());
+             AlertDialog alertdialog = builder.create();
+             alertdialog.show();
+
+             //mScannerView.resumeCameraPreview(this);
+         }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
